ebdeb1b294109e647425f90e6063479f40bf2ef1
lokesh <lokesh.77664@gmail.com>
2026-09-15 17:58:07 +0530

    classwsd

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-function app()  {
+class Robo {
 
-    return(
-        <div>
-            hello world 
-        </div>
-    )
-}
+    constructor(name, colour){
+        this.name = name ;
+        this.colour = colour;
+    }
 
-export default app;
+    sayhi() {
+
+        console.log(" bebeeb  + ", this.name );
+        
+    }
+   
+
+}   
+ const rqwb1= new Robo("lsdai", "blue")
+ const rob1= new Robo("loki", "blue")
+
+export default Robo;
