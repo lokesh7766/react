f7f333c4fed538978e42f19057559dee653043e6
lokesh <lokesh.77664@gmail.com>
2026-09-19 11:19:25 +0530

    mappings

---
+++ index.js
@@ -0,0 +1,3 @@
+const numbers = [ 1,2,3,4];
+const double = numbers.map((num) => num *99);
+console.log(double);
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,3 @@
-import { car } from "./constructor";
-import { car1,car2 } from "./constructor";
-function hello()  {
 
-car1.greet(); 
 
-}
-
-hello();
-export default hello
+export default App
