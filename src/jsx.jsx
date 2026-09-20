058f9ee2222e2922447cf71d3e52d69f280ba0aa
lokesh <lokesh.77664@gmail.com>
2026-09-20 19:29:20 +0530

    jsx

--- src/jsx.jsx
+++ src/jsx.jsx
@@ -1,9 +1,7 @@
-import { createRoot } from 'react-dom/client'
-
 const myElement = <h1>I Love JSX!</h1>;
 
 createRoot(document.getElementById('root')).render(
   myElement
 );
 
-            
+export {myElement};;
